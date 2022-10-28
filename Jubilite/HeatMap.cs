a43316b26c8db9288a3e281d656a47d00b85fc67
Walter Keiler <walterkeiler@n3-32-78.dhcp.drexel.edu>
2022-10-28 10:24:24 -0400

    HeatMap Annotations

--- Jubilite/HeatMap.cs
+++ Jubilite/HeatMap.cs
@@ -9,7 +9,8 @@ public class HeatMap : MonoBehaviour
 {
     [SerializeField] private EnemySpawner enemySpawner;
     public int beatNum;
-    //[SerializeField] private Texture2D output;
+
+    // These are the exposed variables used to dictate how far in front of an enemy we predict and what color that prediction will be
     [Range(0,1)]
     [SerializeField] private float addToGradient = .25f;
     [Range(0,.5f)]
@@ -18,40 +19,47 @@ public class HeatMap : MonoBehaviour
     [SerializeField] private int numberOfCubes = 3;
     public Gradient colorGradient;
     
+    // Holds what enemy to spawn, what beat to spawn it on, and where to spawn it
     private SpawnInfoScriptableObject[] _enemiesToSpawn;
     private List<int> _beatsToSpawnOn;
     private List<int> _beatsToStopOn;
     private List<int> _beatsToSpawnOn2;
     private List<EnemiesToSpawn.SpawnPoints> _spawnedPoints;
     
+    // Holds What beat the enemies spawned on, its current position, its direction, and its behavior/how it will move
     private List<int> _spawnedBeat;
     private List<Vector2> _currentPos;
     private List<Vector2> _currentDir;
     private List<EnemyBehavior> _spawnedBehaviors;
 
+    // Holds the next beat number for each enemy, its next position, its next direction, and its next behavior/how it will move
     private List<int> _nextBeat;
     private List<Vector2> _nextPos;
     private List<Vector2> _nextDir;
     private List<EnemyBehavior> _nextBehaviors;
     
+    // Holds the same as above but for further out from the enemy
     private List<int> _nextBeat2;
     private List<Vector2> _nextPos2;
     private List<Vector2> _nextDir2;
     private List<EnemyBehavior> _nextBehaviors2;
     
+    // I output a 2D array of colors the size of the play area. I used color because I can easily hold multiple levels of information and is easy to visually interpret
     public Color[,] heatMapOutput = new Color[9,5];
 
+    // Holds previous beat's maps
     public List<HeatMapPerBeat> heatMaps = new List<HeatMapPerBeat>(10000);
     
+    // Ideally I would generate these maps before runtime but due to issues with that I am generating them on Awake this has no noticeable performance impact
     private void Awake()
     {
         CreateHeatMap();
     }
 
+    // Reset and initialize all of the arrays that are used to hold all of the enemies, their directions, and their positions.
     [ContextMenu("Reset Arrays")]
     public void ResetArrays()
     {
-
         _spawnedPoints = new List<EnemiesToSpawn.SpawnPoints>();
         _spawnedBeat = new List<int>();
         _currentPos = new List<Vector2>();
@@ -69,13 +77,14 @@ public class HeatMap : MonoBehaviour
         _nextBehaviors2 = new List<EnemyBehavior>();
     }
     
+    // This contains the main loop for the heat map generation
     [ContextMenu("Create Heat Map")]
     public void CreateHeatMap()
     {
-        //heatMapOutput = new Color[9 * beatNum, 5];
 
         heatMaps = new List<HeatMapPerBeat>();
 
+        // This loop sets all positions on the map to a default black
         for (int i = 0; i < beatNum; i++)
         {
             heatMaps.Add(new HeatMapPerBeat());
@@ -85,16 +94,17 @@ public class HeatMap : MonoBehaviour
                 for (int y = 0; y < 5; y++)
                 {
                     heatMaps[i].heatMapOutput[x,y] = Color.black;
-                    //heatMapOutput[x,y] = Color.black;
                 }
             }
         }
         
-        
+        // This takes all the enemies from the spawner and brings them into the _enemiesToSpawn array
         _enemiesToSpawn = enemySpawner._enemiesToSpawn.ToArray();
         _beatsToSpawnOn = new List<int>();
         _beatsToSpawnOn2 = new List<int>();
         _beatsToStopOn = new List<int>();
+
+        // This loop adds all of the beats that we have enemies spawning on.
         for (int i = 0; i < _enemiesToSpawn.Length; i++)
         {
             _beatsToSpawnOn.Add(_enemiesToSpawn[i]._beatToSpawnOn);
@@ -102,8 +112,10 @@ public class HeatMap : MonoBehaviour
             _beatsToStopOn.Add(_enemiesToSpawn[i]._stopRepeatingBeat);
         }
         
+        // We reset all arrays to make sure there is no old data there to mess it up
         ResetArrays();
         
+        // This is the main for loop it emulates the beat of the game and all of the calculations are driven by this
         for (int i = 0; i < beatNum; i++)
         {
             GetEnemies(i);
@@ -113,16 +125,16 @@ public class HeatMap : MonoBehaviour
             if(numberOfCubes > 2)
                 UpdateNextPosition2(i);
             RemoveObj(i);
-            UpdateTexture(i);
+            UpdateMap(i);
         }
-        //Debug.Log("Done");
+
     }
 
+    // This function checks the enemies to see if there is an enemy that is supposed to spawn on it. If there is it and all of its relevant information is added 
     void GetEnemies(int i)
     {
         for (int j = 0; j < _beatsToSpawnOn.Count; j++)
         {
-            //Debug.Log("beat num: " + i + " output " + (i % (_enemiesToSpawn[j]._repeatFrequency + _enemiesToSpawn[j]._beatToSpawnOn)));
             if (_beatsToSpawnOn[j] == i && (i <= _beatsToStopOn[j] || _beatsToStopOn[j] == 0))
             {
                 foreach (var spawn in _enemiesToSpawn[j]._enemiesToSpawn)
@@ -141,6 +153,7 @@ public class HeatMap : MonoBehaviour
                     
                     
                 }
+                // I originally used a % function but there seems to be a bug in C# that caused it to break so this is my work around
                 _beatsToSpawnOn[j] += _enemiesToSpawn[j]._repeatFrequency;
             }
 
@@ -158,6 +171,7 @@ public class HeatMap : MonoBehaviour
         }
     }
 
+    // This function drives movement and checks each enemies movement behavior to see what it is supposed to do this beat
     void UpdatePosition(int i)
     {
         for (int j = 0; j < _spawnedBeat.Count; j++)
@@ -176,6 +190,7 @@ public class HeatMap : MonoBehaviour
         
     }
 
+    // This function does the same as above but for predicting further into the future
     void UpdateNextPosition(int i)
     {
 
@@ -195,7 +210,8 @@ public class HeatMap : MonoBehaviour
         }
         
     }
-    
+
+    // This function does the same as above but for predicting further into the future
     void UpdateNextPosition2(int i)
     {
         
@@ -216,23 +232,18 @@ public class HeatMap : MonoBehaviour
         
     }
     
-    void UpdateTexture(int i)
+    // This function updates the output array with the position of the enmy and the prediction of where it is going
+    // I use += so that if enemies are going to cross paths those squares will be more intense
+    void UpdateMap(int i)
     {
         foreach (var pos in _currentPos)
         {
             
-            if (Mathf.Abs(pos.x) > 8 || pos.x < 0 || Mathf.Abs(pos.y) > 5 || pos.y >= 0)
-            {
-
-            }
-            else
+            if (!Mathf.Abs(pos.x) > 8 || !pos.x < 0 || !Mathf.Abs(pos.y) > 5 || !pos.y >= 0)
             {
                 heatMaps[i].heatMapOutput[(int) pos.x, (int) -pos.y - 1].r += (addToGradient + (nextPosColorOffset * 2));
-                //heatMapOutput[(int) pos.x + (i * 9), (int) -pos.y - 1].r += (addToGradient + (nextPosColorOffset * 2));
                 heatMaps[i].heatMapOutput[(int) pos.x, (int) -pos.y - 1].b += (addToGradient + (nextPosColorOffset * 4));
-                //heatMapOutput[(int) pos.x + (i * 9), (int) -pos.y - 1].b += (addToGradient + (nextPosColorOffset * 4));
                 heatMaps[i].heatMapOutput[(int) pos.x, (int) -pos.y - 1].g += (addToGradient + (nextPosColorOffset * 4));
-                //heatMapOutput[(int) pos.x + (i * 9), (int) -pos.y - 1].g += (addToGradient + (nextPosColorOffset * 4));
             }
         }
         
@@ -241,10 +252,7 @@ public class HeatMap : MonoBehaviour
             foreach (var pos in _nextPos)
             {
                 bool test = true;
-                if (Mathf.Abs(pos.x) > 8 || pos.x < 0 || Mathf.Abs(pos.y) > 5 || pos.y >= 0)
-                {
-                }
-                else
+                if (!Mathf.Abs(pos.x) > 8 || !pos.x < 0 || !Mathf.Abs(pos.y) > 5 || !pos.y >= 0)
                 {
                     for (int j = 0; j < _nextBeat.Count; j++)
                     {
@@ -267,10 +275,7 @@ public class HeatMap : MonoBehaviour
             foreach (var pos in _nextPos2)
             {
                 bool test = true;
-                if (Mathf.Abs(pos.x) > 8 || pos.x < 0 || Mathf.Abs(pos.y) > 5 || pos.y >= 0)
-                {
-                }
-                else
+                if (!Mathf.Abs(pos.x) > 8 || !pos.x < 0 || !Mathf.Abs(pos.y) > 5 || !pos.y >= 0)
                 {
                     for (int j = 0; j < _nextBeat2.Count; j++)
                     {
@@ -287,16 +292,9 @@ public class HeatMap : MonoBehaviour
                 }
             }
         } 
-        
-        //Debug.Log("Beat: " + i + "\n" +
-        //            heatMapOutput[(0 + (i * 9)), (int) 0].r + " , " + heatMapOutput[1 + (i * 9), (int) 0].r + " , " + heatMapOutput[2 + (i * 9), (int) 0].r + " , " + heatMapOutput[3 + (i * 9), (int) 0].r + " , " + heatMapOutput[4 + (i * 9), (int) 0].r + " , " + heatMapOutput[5 + (i * 9), (int) 0].r + " , " + heatMapOutput[6 + (i * 9), (int) 0].r + " , " + heatMapOutput[7 + (i * 9), (int) 0].r + " , " + heatMapOutput[8 + (i * 9), (int) 0].r + "\n" +
-        //            heatMapOutput[0 + (i * 9), (int) 1].r + " , " + heatMapOutput[1 + (i * 9), (int) 1].r + " , " + heatMapOutput[2 + (i * 9), (int) 1].r + " , " + heatMapOutput[3 + (i * 9), (int) 1].r + " , " + heatMapOutput[4 + (i * 9), (int) 1].r + " , " + heatMapOutput[5 + (i * 9), (int) 1].r + " , " + heatMapOutput[1 + (i * 9), (int) 1].r + " , " + heatMapOutput[7 + (i * 9), (int) 1].r + " , " + heatMapOutput[8 + (i * 9), (int) 1].r + "\n" +
-        //            heatMapOutput[0 + (i * 9), (int) 2].r + " , " + heatMapOutput[1 + (i * 9), (int) 2].r + " , " + heatMapOutput[2 + (i * 9), (int) 2].r + " , " + heatMapOutput[3 + (i * 9), (int) 2].r + " , " + heatMapOutput[4 + (i * 9), (int) 2].r + " , " + heatMapOutput[5 + (i * 9), (int) 2].r + " , " + heatMapOutput[1 + (i * 9), (int) 2].r + " , " + heatMapOutput[7 + (i * 9), (int) 2].r + " , " + heatMapOutput[8 + (i * 9), (int) 2].r + "\n" +
-        //            heatMapOutput[0 + (i * 9), (int) 3].r + " , " + heatMapOutput[1 + (i * 9), (int) 3].r + " , " + heatMapOutput[2 + (i * 9), (int) 3].r + " , " + heatMapOutput[3 + (i * 9), (int) 3].r + " , " + heatMapOutput[4 + (i * 9), (int) 3].r + " , " + heatMapOutput[5 + (i * 9), (int) 3].r + " , " + heatMapOutput[1 + (i * 9), (int) 3].r + " , " + heatMapOutput[7 + (i * 9), (int) 3].r + " , " + heatMapOutput[8 + (i * 9), (int) 3].r + "\n" +
-        //            heatMapOutput[0 + (i * 9), (int) 4].r + " , " + heatMapOutput[1 + (i * 9), (int) 4].r + " , " + heatMapOutput[2 + (i * 9), (int) 4].r + " , " + heatMapOutput[3 + (i * 9), (int) 4].r + " , " + heatMapOutput[4 + (i * 9), (int) 4].r + " , " + heatMapOutput[5 + (i * 9), (int) 4].r + " , " + heatMapOutput[1 + (i * 9), (int) 4].r + " , " + heatMapOutput[7 + (i * 9), (int) 4].r + " , " + heatMapOutput[8 + (i * 9), (int) 4].r + "\n");
-
     }
 
+    // This function removes objects from the arrays once they leave the board
     void RemoveObj(int i)
     {
         int length = _spawnedPoints.Count;
@@ -363,6 +361,7 @@ public class HeatMap : MonoBehaviour
         }
     }
     
+    // This Vector2 gives the position an enemy will spawn based on the SpawnPoint Enum
     Vector2 StartPosition(EnemiesToSpawn.SpawnPoints _spawnPoint)
     {
         int x = 0, y = 0;
@@ -484,6 +483,7 @@ public class HeatMap : MonoBehaviour
         return new Vector2(x,y);
     } 
     
+    // This Vector2 gives the direction an enemy will spawn based on the SpawnPoint Enum
     Vector2 StartDirection(EnemiesToSpawn.SpawnPoints _spawnPoint)
     {
         int x = 0, y = 0;
@@ -605,6 +605,7 @@ public class HeatMap : MonoBehaviour
         return new Vector2(x,y);
     }
 
+    // This Vector2 gives the direction a enemy will turn on the Direction Enum in EnemyBehavior
     Vector2 ChangeDirection(Direction _direction, Vector2 currentDir)
     {
         int x = 0, y = 0;
